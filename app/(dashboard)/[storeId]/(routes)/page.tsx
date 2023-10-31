4109e72f120a7af72dbb6079212425455b68992e
Brian Shimkus <brianshimkus1@gmail.com>
2023-10-31 18:26:20 -0500

    Pulled in store name into dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,22 @@
-export default function DashboardPage() {
-	return <div>Dashboard</div>
+import prismadb from '@/lib/prismadb'
+
+interface DashboardPageProps {
+	params: {
+		storeId: string
+	}
 }
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+	const store = await prismadb.store.findFirst({
+		where: {
+			id: params.storeId,
+		},
+	})
+
+	return (
+		<>
+			<h1>{store?.name}</h1>
+		</>
+	)
+}
+export default DashboardPage
