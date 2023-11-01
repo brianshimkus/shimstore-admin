eb42bcd354a63b4a9dca6d071c5a05e38b73fdf7
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-01 17:00:26 -0500

    Built API alert section

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { AlertModal } from '@/components/modals/alert-modal'
+import { ApiAlert } from '@/components/ui/api-alert'
 import { Button } from '@/components/ui/button'
 import {
 	Form,
@@ -128,6 +129,11 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 				</form>
 			</Form>
 			<Separator />
+			<ApiAlert
+				title='NEXT_PUBLIC_API_URL'
+				description={`${origin}/api/${params.storeId}}`}
+				variant='public'
+			/>
 		</>
 	)
 }
